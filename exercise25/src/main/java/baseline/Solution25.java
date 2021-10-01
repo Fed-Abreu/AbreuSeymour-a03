d2d94fd841499592007f52656696919ca0f31781
Fed-Abreu <arielsey@hotmail.com>
2021-10-01 19:55:52 -0400

    exercise25 finish

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -1,19 +1,50 @@
 package baseline;
+
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution25 {
-    public void passwordValidator(){
+    String passwordStrength;
+    public String passwordValidator(String password){
+        //If password is ONLY Numbers and is < 8 on length Then Display it is a VERY WEAK password
+        if(password.matches("[0-9]+") && password.length() < 8){
+            passwordStrength = "very weak password.";
+        }
+        //If password is ONLY Letters and is < 8 on length THen Display it is a WEAK password
+        else if(password.matches("[a-zA-Z]+") && password.length() < 8){
+            passwordStrength = "weak password.";
+        }
+        //If password is BOTH Letters and AT LEAST ONE Number  and is >= 8 on length Then Display it is a STRONG password
+        else if(password.matches("[0-9a-zA-Z]+") && password.length() >= 8){
+            passwordStrength = "strong password.";
+        }
+        //If password contains Letters, Numbers, and Special Characters and is >= 8 Then Display it is a VERY STRONG password
+        else if(password.matches("[0-9a-zA-Z!@#&()–[{}]:;',?/*~$^+=<>]+") && password.length() >= 8) {
+            passwordStrength = "very strong password.";
+        }
+        //else Display password has an unknown strength
+        else {
+            passwordStrength = "unknown password strength.";
 
+        }
+
+        return passwordStrength;
     }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Solution25 sol = new Solution25();
         //Display To enter a password and Scan Password as String
-        //If password is ONLY Numbers and is < 8 on length Then Display it is a VERY WEAK password
-        //If password is ONLY Letters and is < 8 on length THen Display it is a WEAK password
-        //If password is BOTH Letters and ATLEAST ONE Number  and is >= 8 on length Then Display it is a STRONG password
-        //If password contains Letters, Numbers, and Special Characters and is >= 8 Then Display it is a VERY STRONG password
-        //else Display password has an unknown strength
+        System.out.print("Please enter a password: ");
+        String password = input.nextLine();
+
+        sol.passwordValidator(password);
+
+        System.out.println("The password '"+ password + "' is a " + sol.passwordStrength);
+
+
 
     }
 }
--- exercise25/src/test/java/baseline/Solution25Test.java
+++ exercise25/src/test/java/baseline/Solution25Test.java
@@ -1,7 +1,60 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution25Test {
+    Solution25 sol = new Solution25();
+    @Test
+    void test_passwordValidator_VeryWeak(){
+
+        String password = "12345";
+
+        String actual_password = sol.passwordValidator(password);
+        String expected_password = "very weak password.";
+
+        assertEquals(expected_password,actual_password);
+    }
+    @Test
+    void test_passwordValidator_Weak(){
+
+        String password = "abcdef";
+
+        String actual_password = sol.passwordValidator(password);
+        String expected_password = "weak password.";
+
+        assertEquals(expected_password,actual_password);
+    }
+    @Test
+    void test_passwordValidator_Strong(){
+
+        String password = "abc123xyz";
+
+        String actual_password = sol.passwordValidator(password);
+        String expected_password = "strong password.";
+
+        assertEquals(expected_password,actual_password);
+    }
+    @Test
+    void test_passwordValidator_VeryStrong(){
+
+        String password = "1337h@xor!";
+
+        String actual_password = sol.passwordValidator(password);
+        String expected_password = "very strong password.";
+
+        assertEquals(expected_password,actual_password);
+    }
+    @Test
+    void test_passwordValidator_Unknown(){
+
+        String password = "";
+
+        String actual_password = sol.passwordValidator(password);
+        String expected_password = "unknown password strength.";
+
+        assertEquals(expected_password,actual_password);
+    }
 
 }
