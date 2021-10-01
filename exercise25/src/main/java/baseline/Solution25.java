ca88a13c8ec28da6e05f9d73a720b317659cd597
Fed-Abreu <arielsey@hotmail.com>
2021-10-01 17:42:49 -0400

    excercise25 part1

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution25 {
+    public void passwordValidator(){
+
+    }
+    public static void main(String[] args) {
+        //Display To enter a password and Scan Password as String
+        //If password is ONLY Numbers and is < 8 on length Then Display it is a VERY WEAK password
+        //If password is ONLY Letters and is < 8 on length THen Display it is a WEAK password
+        //If password is BOTH Letters and ATLEAST ONE Number  and is >= 8 on length Then Display it is a STRONG password
+        //If password contains Letters, Numbers, and Special Characters and is >= 8 Then Display it is a VERY STRONG password
+        //else Display password has an unknown strength
+
+    }
+}
