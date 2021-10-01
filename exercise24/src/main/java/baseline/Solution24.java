d0dbb2b304e20d469ce9e0decd47f5e6042493e0
Fed-Abreu <arielsey@hotmail.com>
2021-10-01 16:17:25 -0400

    exercise24 part 1

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,26 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution24 {
+    //calculate Anagram using boolean
+    public void isAnagram(){
+        //If first string doesn't have same length return false
+        //ELSE
+        //Create a char[] array = to BOTH the strings
+        //Then array.sort BOTH
+        //then return array.equals(arrays)
+
+    }
+
+    public static void main(String[] args) {
+        //Display Enter two strings, and I'll tell if they are anagrams:
+        //Display Enter the first string: Scan the user input as a String
+        //Display Enter the second string: Scan in as String
+        //If true Display "string 1 and string 2 are anagrams
+        //else Display string 1 adn string2  are not anagrams
+
+    }
+}
