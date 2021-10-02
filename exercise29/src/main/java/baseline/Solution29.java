c0a78943fd110f867c97ca61d72b9e82b314ec9b
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 04:27:14 -0400

    exercise29 finished

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,17 +1,38 @@
 package baseline;
 
+import java.util.Scanner;
+
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution29 {
-    public void loopCalculator(){
-    }
-    public static void main(String[] args) {
-        //throw an exception
+    //throw an exception
+    public String loopCalculator(){
+        Scanner input = new Scanner(System.in);
+        int number;
         //while true Display What is the rate of return Scan as int
-        //if number is >= 1 Then print it will take 72/number year to doulbe investment then break
-        //else display this is not a valid input
+        while(true) {
+            try {
+                System.out.print("What is the rate of return? ");
+                number = Integer.parseInt(input.next());
+                //Then print it will take 72/number year to doulbe investment
+                System.out.println("It will take " + 72 / number + " years to double your initial investment.");
+                break;
+                //NumberFormatException and Arithmetic Exception
+            }catch (NumberFormatException | ArithmeticException e){
+                System.out.println("Sorry. That's not a valid input");
+            }
+
+        }
+
+        return null;
+    }
+    public static void main(String[] args){
+        Solution29 sol = new Solution29();
+
+        sol.loopCalculator();
 
     }
 }
--- exercise29/src/test/java/Solution29Test.java
+++ exercise29/src/test/java/Solution29Test.java
@@ -1,5 +1,27 @@
+import baseline.Solution29;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution29Test {
+    Scanner input = new Scanner(System.in);
+    Solution29 sol = new Solution29();
+@Test
+    void test_loopCalculatorWorks(){
+        int number = 4;
+
+        String actualResult = "It will take 18 years to double your initial investment.";
+
+        String expectedResult = "It will take " + 72 / number + " years to double your initial investment.";
+
+    assertEquals(expectedResult,actualResult);
+
+
+
+
+
+    }
 
 }
