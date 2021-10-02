fffcab266812c608b8f21d189cb3ffcbbc9c3321
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 19:26:18 -0400

    exercise33 part 1

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution33 {
+    public void magicBallResponses(){
+
+    }
+    public static void main(String[] args) {
+        //Display What is your question? and Scan String
+        //String reponses = Yes,No,Maybe,Ask again later
+        //Make a new object Random rand Random()
+        //Choose an index using responseIndex = random.nextInt(Length)
+        //Display Response using number generator
+        //Then make response = responseIndex
+        //Display the response from magicBall
+    }
+}
