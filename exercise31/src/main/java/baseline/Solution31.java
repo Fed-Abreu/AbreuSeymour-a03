079d5df727a33b063a43fd57e98a234e7045ddcc
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 16:44:50 -0400

    exercise31 finished

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -1,19 +1,54 @@
 package baseline;
 
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
 public class Solution31 {
+
     public void heartRateCalculator(){
-        //Calculate heart rate using TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+        Scanner input = new Scanner(System.in);
+        //Try loop Until the user enters a valid heart rate and age
+            try {
+                //Display Enter Resting Heat Rate: and Scan int
+                System.out.print("Enter resting heart rate: ");
+                int restingPulse = input.nextInt();
+
+                //Display Enter Age: and Scan int
+                System.out.print("Enter Age: ");
+                 int age = input.nextInt();
+
+                //Display Resting Pulse: and Age:
+
+                System.out.println("\nResting Pulse " + restingPulse + "        " + "Age: " + age + "\n");
+
+                //Display Intensity    | Rate
+                System.out.println("Intensity    | Rate");
+                //Display -------------|--------
+                System.out.println("-------------|--------");
+
+                //For(int intensity = 55 intensity <=95 intensity += 5
+                for(int intensity = 55; intensity <= 95; intensity +=5){
+                    //Calculate heart rate using TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+
+                    int targetHeartRate = (((220 - age) - restingPulse) * intensity/100) + restingPulse;
+                    //Display intensity%          | targetHeartRate bpm
+                    System.out.println(intensity + "%" +"          | " + targetHeartRate + " bpm");
+                }
+                //Catch Exception e and Display enter only numbers
+            } catch (Exception e) {
+                System.out.println("This is not a valid input. ");
+            }
+
+
     }
     public static void main(String[] args) {
-        //Try loop Until the user enters a valid heart rate and age
-        //Display Resting Heat Rate: and Scan int
-        //Display Enter Age: and Scan int
-        //Display Resting Pulse: and Age:
-        //Catch Exception e and Display enter only numbers
-        //Display Intensity    | Rate
-        //Display -------------|--------
-        //For(int intensity = 55 intensity <=95 intensity += 5
-        //Display intensity%          | targetHeartRate bpm
+
+        Solution31 sol = new Solution31();
+        sol.heartRateCalculator();
+
 
 
 
--- exercise31/src/test/java/baseline/Solution31Test.java
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -1,7 +1,27 @@
 package baseline;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution31Test {
+    Solution31 sol = new Solution31();
+    @Test
+    void test_CalculatorValid(){
+
+
+        int age = 22;
+        int restingPulse = 65;
+        int intensity = 55;
+
+        int targetHeartRate = (((220 - age) - restingPulse) * intensity/100) + restingPulse;
+
+        assertEquals(targetHeartRate, 138);
+
+    }
+
 
 }
