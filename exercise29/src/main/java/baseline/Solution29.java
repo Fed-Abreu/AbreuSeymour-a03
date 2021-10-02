eb35d81f9181258818a37ff025abf3601179b551
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 03:31:42 -0400

    exercise29 part 1

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,17 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution29 {
+    public void loopCalculator(){
+    }
+    public static void main(String[] args) {
+        //throw an exception
+        //while true Display What is the rate of return Scan as int
+        //if number is >= 1 Then print it will take 72/number year to doulbe investment then break
+        //else display this is not a valid input
+
+    }
+}
