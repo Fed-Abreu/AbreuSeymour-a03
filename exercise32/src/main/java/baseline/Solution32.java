ec366862dbf9d44959fff20bb95bafa447a907dc
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 17:33:26 -0400

    exercise32 part 1

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,33 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution32 {
+    public void difficultyOfNumber() {
+
+    }
+    public static void main(String[] args) {
+        //while true try
+        //Display Let's play Guess the Number!
+        //Display Enter difficulty level (1,2,3): Scan int
+        //Catch Exception e Display Please Enter a valid input:
+
+
+        //if 1 play from number 1-10
+        //set random number from range math.random*10+1
+        //else if 2 play from numbers 1-100
+        //set random number from range math.random*100+1
+        //elseif 3 play from numbers 1-1000
+        //set random number from range math.random*1000+1
+        //Display I have my number What's your guess?:
+        //do Scan input as int
+        //totalTrys ++
+        //If Guess == RandomNumber Display You got it in "total" guesses!
+        //else if guess < RandomNumber Display Too low. Guess again:
+        //else if guess > RandomNumber Display Too high. Guess again:
+        //while guess != RandomNumber Display you wish to play again (Y/N)? Scan as Char
+        //if choice is N or n break
+
+    }
+}
