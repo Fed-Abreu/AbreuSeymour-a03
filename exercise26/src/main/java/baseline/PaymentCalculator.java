91f2096e904152b9781a597b510f4eb1825f192d
Fed-Abreu <arielsey@hotmail.com>
2021-10-01 22:12:49 -0400

    exercise26 part 1

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class PaymentCalculator {
+    public void calculateMonthsUntilPaidOff(){
+        //Calculate number of months using
+        //n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+        //round the number of months
+        //return the number of months
+    }
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,15 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution26 {
+    //n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+    public static void main(String[] args) {
+        //Display What is your Balance? Scan as double
+        //Display What is your APR (as a percent)? Scan as double and divide by 100 to make it a percent
+        //Display What is the monthly payment you can make? Scan double
+        //Display It will take n number of months to pay off this card.
+
+    }
+}
