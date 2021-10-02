e85e8f15407033ff324aade1336b112d74e60436
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 04:47:09 -0400

    exercise 30 part 1

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution30 {
+    public void multiplicationTable(){
+
+    }
+    public static void main(String[] args) {
+        Solution30 sol = new Solution30();
+        //for int i=1 i<+12 i++
+        //for int j=1 j<=12 i ++
+        //System.out.format %4d Display I*j
+        //Print new line
+
+    }
+}
