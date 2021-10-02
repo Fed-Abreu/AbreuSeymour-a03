b726bed72032aac774e7e2506623e08ad2d54a23
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 15:12:28 -0400

    exercise31 part 1

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,21 @@
+package baseline;
+
+public class Solution31 {
+    public void heartRateCalculator(){
+        //Calculate heart rate using TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+    }
+    public static void main(String[] args) {
+        //Try loop Until the user enters a valid heart rate and age
+        //Display Resting Heat Rate: and Scan int
+        //Display Enter Age: and Scan int
+        //Display Resting Pulse: and Age:
+        //Catch Exception e and Display enter only numbers
+        //Display Intensity    | Rate
+        //Display -------------|--------
+        //For(int intensity = 55 intensity <=95 intensity += 5
+        //Display intensity%          | targetHeartRate bpm
+
+
+
+    }
+}
