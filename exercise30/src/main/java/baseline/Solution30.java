afa695676dcd7e1753fc4ef810da29deb1283b6f
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 04:56:24 -0400

    exercise 30 finished

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -4,15 +4,23 @@ package baseline;
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution30 {
-    public void multiplicationTable(){
-
+    public int multiplicationTable(){
+        //for int i=1 i<+12 i++
+        for (int i = 1; i <= 12; i++) {
+            //for int j=1 j<=12 i ++
+            for (int j = 1; j <= 12; j++) {
+                //System.out.format Display I*j
+                System.out.format("%4d", i * j);
+            }
+            //Print new line
+            System.out.println();
+        }
+        return 0;
     }
     public static void main(String[] args) {
         Solution30 sol = new Solution30();
-        //for int i=1 i<+12 i++
-        //for int j=1 j<=12 i ++
-        //System.out.format %4d Display I*j
-        //Print new line
+
+        sol.multiplicationTable();
 
     }
 }
--- exercise30/src/test/java/baseline/Solution30Test.java
+++ exercise30/src/test/java/baseline/Solution30Test.java
@@ -1,7 +1,21 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution30Test {
+    Solution30 sol = new Solution30();
+    @Test
+    void test_TimesTable(){
+        int i = 0;
+        int j = 0 ;
+        sol.multiplicationTable();
+
+        int expectedResults = i*j;
+
+        assertEquals(expectedResults,sol.multiplicationTable());
+
+    }
 
 }
