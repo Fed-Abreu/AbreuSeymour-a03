76fea2a397d731671a2dfaddca50762d21376bf4
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 02:30:00 -0400

    exercise28 part 1

--- exercise27/src/test/java/Solution27Test.java
+++ exercise27/src/test/java/Solution27Test.java
@@ -73,7 +73,7 @@ class Solution27Test {
 
         assertEquals(false, actualEmployeeID);
     }
-   
+
 
 
 
---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution28 {
+    public void calculateTotal(){
+    }
+    public static void main(String[] args) {
+        //make total = 0
+        //for i = 0 i<5 i++
+        //Display Enter a number: and Scan as int
+        //total += int
+        //Print out total
+
+    }
+
+}
