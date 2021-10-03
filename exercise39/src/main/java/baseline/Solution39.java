13c4b5b80222ccbe7a0fa04cb900acf33c3f1746
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 18:59:47 -0400

    exercise39 part 1

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,22 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+
+public class Solution39 {
+    public void Employee(){
+
+    }
+    public static void main(String[] args) {
+        //Make Private String variables for firstName lastName Position and separationDate
+        //Then make constructor for variables
+        //Make them in Employee
+        //Make get and set methods for variables
+        //Sort by last name
+        //Create an Array to make a new Employee
+        //add new Employee to list
+        //Display employee with correct table format
+        
+    }
+}
