1742e8090d620ff99b050db4bd47da816016836e
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 16:43:59 -0400

    exercise37 finished

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -4,19 +4,58 @@ package baseline;
  *  Copyright 2021 Federico Abreu Seymour
  */
 
-public class Solution37 {
-    public void passwordGenerator(){
+import java.util.*;
 
+
+public class Solution37 {
+
+    public String passwordGenerator(int length, int specialCharactersAmount,int numbersAmount){
+        Random rand = new Random();
+        //Create an ArrayList of those Characters
+        ArrayList<Character>characters = new ArrayList<>();
+        //String = all the values of possible Characters
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String specialCharacters = "!@#$%^&*_-+=?/:<,>.{}[]';";
+        String numbers = "1234567890";
+
+        //Loop Each Character to add random Character to the Array
+        for(int i = 0; i < specialCharactersAmount; i++){
+            characters.add(specialCharacters.charAt(rand.nextInt(specialCharacters.length())));
+        }
+        for(int i = 0; i < numbersAmount; i++){
+            characters.add(numbers.charAt(rand.nextInt(numbers.length())));
+        }
+        for(int i = characters.size()-1; i < length; i++){
+            characters.add(letters.charAt(rand.nextInt(letters.length())));
+        }
+        //randomize order
+        Collections.shuffle(characters);
+
+        //Add that to the Password String
+        StringBuilder password = new StringBuilder();
+        for (Character c: characters){
+            password.append(Character.toString(c));
+        }
+        return password.toString();
     }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Solution37 sol = new Solution37();
         //Display What's the minimum length? and Scan int
+        System.out.print("What is the minimum length? ");
+        int length = Integer.parseInt(input.nextLine());
+
         //Display How many special characters? and Scan int
+        System.out.print("How many special characters? ");
+        int specialCharactersAmount = Integer.parseInt(input.nextLine());
+
         //Display How many numbers? Scan int
-        //String = all the values of possible Characters
-        //Create an ArrayList of those Characters
-        //Loop Each Character to add random Character to the Array
-        //Add that to the Password String
+        System.out.print("How many numbers? ");
+        int numbersAmount = Integer.parseInt(input.nextLine());
+        sol.passwordGenerator(length,specialCharactersAmount,numbersAmount);
+
         //Display Your password is "password"
+        System.out.println("Your password is " + sol.passwordGenerator(length,specialCharactersAmount,numbersAmount));
 
     }
 }
--- exercise37/src/test/java/baseline/Solution37Test.java
+++ exercise37/src/test/java/baseline/Solution37Test.java
@@ -1,7 +1,29 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution37Test {
+    Solution37 sol = new Solution37();
+    @Test
+    void test_DisplaysPassword(){
+        ArrayList<Character> characters = new ArrayList<>();
+       String acutalResults = sol.passwordGenerator(8,2,2);
+       String expectedResults = "";
+
+       assertNotEquals(expectedResults,acutalResults);
+    }
+    @Test
+    void test_ContainsTwoNumbersAndTwoSpecial() {
+
+        ArrayList<Character> characters = new ArrayList<>();
+        String Results = sol.passwordGenerator(8, 2, 2);
+        if(Results.matches("[!@#$%^&*_+=?/:<,>.{}';]")) {
+            assertEquals(true, false);
+        }
+    }
 
 }
