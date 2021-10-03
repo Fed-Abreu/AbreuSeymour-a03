ca90d7d76e836f5ad4f9fff72b79c4de78ac81b4
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 02:41:09 -0400

    exercise36  part 1

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,42 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution36 {
+    public void average(){
+
+    }
+    public void max(){
+
+    }
+    public void min(){
+
+    }
+    public void std(){
+
+    }
+    public static void main(String[] args) {
+        //Create an ArrayList for numbers make them integers
+        //do a while number != done loop
+        //Display Enter a Number and Scan int
+        //if int does not equal done
+        //Then int num Integer parse int
+        //numbers add (int)
+        //catchException e
+        //end while loop number != done
+        //calculate average for num:numbers if average = total / number size
+        //calculate maximum for num:numbers if num > max max = num
+        //calculate minimum for num:numbers if num < min min = num
+        //calculate std for num: numbers sum = sum + Math.pow num - average, 2
+        //Then Math.sqrt sum/number size
+        //Display Numbers: All the numbers collected from Array
+        //Display The average is average
+        //Display The minimum is minimum
+        //Display The standard deviation is std
+
+
+
+
+    }
+}
