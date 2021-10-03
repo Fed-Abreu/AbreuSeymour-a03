a05479cddd7fa0a544a08cb034ac8dd2169bb2c2
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 21:41:44 -0400

    exercise34 part 1

---
+++ exerccise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution34 {
+    public void employeeList(){
+        
+    }
+    public static void main(String[] args) {
+        //Create a new ArrayList with employees John Smith, Jackie Jackson, Chris Jones, Amanda Cullen, Jeremy Goodwin
+        //Display There are "number in array" employees:
+        // Then display the ArrayList
+        //Then Display Enter an employee name to remove: Scan as a String
+        //Then remove employee("String")
+        //Then Display the new list of employees
+
+    }
+}
