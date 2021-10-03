cfd7b9381a30fd3f1df9a9468ade719ffb90af25
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 20:09:04 -0400

    exercise33 finished

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -1,19 +1,49 @@
 package baseline;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution33 {
-    public void magicBallResponses(){
+    //Make a new object Random rand Random()
+    private final Random rand = SecureRandom.getInstanceStrong();
 
+    public Solution33() throws NoSuchAlgorithmException {
     }
-    public static void main(String[] args) {
-        //Display What is your question? and Scan String
-        //String reponses = Yes,No,Maybe,Ask again later
-        //Make a new object Random rand Random()
+
+    public String magicBallResponses(){
+
+
+        //String responses = Yes,No,Maybe,Ask again later
+        String[] responses = {"Yes","No","Maybe","Ask again later"};
+
         //Choose an index using responseIndex = random.nextInt(Length)
-        //Display Response using number generator
+        int responseIndex = this.rand.nextInt(responses.length);
+
         //Then make response = responseIndex
+        String response = responses[responseIndex];
+
         //Display the response from magicBall
+        System.out.println("\n" + response + ".");
+
+        return response;
+    }
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner input = new Scanner(System.in);
+        Solution33 sol = new Solution33();
+
+        //Display What is your question? and Scan String
+        System.out.print("What's your question? \n> ");
+        String question = input.nextLine();
+
+
+        sol.magicBallResponses();
+
+
     }
 }
--- exercise33/src/test/java/baseline/Solution33Test.java
+++ exercise33/src/test/java/baseline/Solution33Test.java
@@ -1,7 +1,24 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution33Test {
+    Solution33 sol = new Solution33();
+
+    Solution33Test() throws NoSuchAlgorithmException {
+    }
+
+    @Test
+    void test_magicbalContainsRandomWord(){
+        String actualResult = sol.magicBallResponses();
+
+        String[] expecetedResults = {"Yes","No","Maybe","Ask again later"};
+
+        assertEquals(expecetedResults,actualResult);
+    }
 
 }
