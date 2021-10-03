2599b4d1d9e17da956270c320968ad7e0580acc3
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 22:51:47 -0400

    exercise35 part 1

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Solution35 {
+    public void nameWinner(){
+
+    }
+    public static void main(String[] args) {
+        //Create an ArrayList name = new ArrayList
+        //do a loop while name isnt blank
+        //if the name isnt blank add name to the ArrayList
+        //Then select a random name using new Random
+        //Then winner = random.nextInt(name.size)
+        //Display The winner is... winner
+    }
+}
