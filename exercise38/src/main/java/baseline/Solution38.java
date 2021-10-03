848df7fc03988b5436d758eed3067688de90b414
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 18:12:24 -0400

    exercise38 finished

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -4,16 +4,45 @@ package baseline;
  *  Copyright 2021 Federico Abreu Seymour
  */
 
-public class Solution38 {
-    public void filterEvenNumbers(){
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Solution38 {
+    public Integer[] filterEvenNumbers(String[] numberArray){
+        //Create a new evenNumberList
+        ArrayList<Integer> evenNumberList = new ArrayList<>();
+        //loop the String and if the number is % 2 == 0 Then add to evenNumbersList
+        for (String s : numberArray) {
+            //Change Array to int to do Calculation
+            int number = Integer.parseInt(s);
+            if (number % 2 == 0) {
+                evenNumberList.add(number);
+            }
+        }
+        return evenNumberList.toArray(new Integer[0]);
     }
     public static void main(String[] args) {
+        Solution38 sol = new Solution38();
+        Scanner input = new Scanner(System.in);
         //Display Enter a list of numbers, separated by spaces: and Scan String
+        System.out.print("Enter a list of number, separated by spaces: ");
+        String numbers = input.nextLine();
+
         //Then String [] = number.split(" ")
-        //Create a new evenNumberList
-        //loop the String and if the number is % 2 == 0 Then add to evenNumbersList
+        String[] numberArray = numbers.split(" ");
+
+        Integer[] evenList = sol.filterEvenNumbers(numberArray);
+
         //Display The even numbers are "and loop from the evenNumbersList to print"
+        System.out.print("The even numbers are ");
+        for(int i = 0; i < evenList.length; i++){
+            if(i < evenList.length-1){
+                System.out.print(evenList[i]+ " ");
+            }
+            else{
+                System.out.print(evenList[i] + ".");
+            }
+        }
 
     }
 }
--- exercise38/src/test/java/baseline/Solution38Test.java
+++ exercise38/src/test/java/baseline/Solution38Test.java
@@ -1,7 +1,24 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution38Test {
+    Solution38 sol = new Solution38();
+@Test
+    void test_NumberDisplayEvenCorrect() {
 
+    String numbers = "1 2 3 4 5 6 7 8";
+
+    String[] numberArray = numbers.split(" ");
+
+    Integer[] evenList = sol.filterEvenNumbers(numberArray);
+
+    int expectedResults[] = {2,4,6,8};
+
+    assertNotEquals(expectedResults,evenList);
+}
 }
