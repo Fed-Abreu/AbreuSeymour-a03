2bbf337872c2e8be8b209a634ff8d8b581e54be0
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 15:16:49 -0400

    exercise37 part 1

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,22 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+
+public class Solution37 {
+    public void passwordGenerator(){
+
+    }
+    public static void main(String[] args) {
+        //Display What's the minimum length? and Scan int
+        //Display How many special characters? and Scan int
+        //Display How many numbers? Scan int
+        //String = all the values of possible Characters
+        //Create an ArrayList of those Characters
+        //Loop Each Character to add random Character to the Array
+        //Add that to the Password String
+        //Display Your password is "password"
+
+    }
+}
