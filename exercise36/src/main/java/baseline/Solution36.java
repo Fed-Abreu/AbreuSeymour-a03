4e869f4260afe29ac2b63d37de56e60a069a97ed
Fed-Abreu <arielsey@hotmail.com>
2021-10-03 03:41:44 -0400

    exercise36 finished

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -1,42 +1,95 @@
 package baseline;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Federico Abreu Seymour
  */
 public class Solution36 {
-    public void average(){
+    public double average(ArrayList<Integer>numbers){
+        //calculate average for num:numbers if average = total / number size
+        int total = 0;
+        for(int num: numbers){
+            total += num;
+        }
+        double averageNumber = (double)(total)/ numbers.size();
+        return averageNumber;
 
     }
-    public void max(){
-
+    public int max(ArrayList<Integer>numbers){
+        //calculate maximum for num:numbers if num > max max = num
+        int max = numbers.get(0);
+        for(int num:numbers){
+            if(num > max)
+                max = num;
+        }
+        return max;
     }
-    public void min(){
-
+    public int min(ArrayList<Integer>numbers){
+     //calculate minimum for num:numbers if num < min min = num
+        int min = numbers.get(0);
+        for(int num:numbers){
+            if(num < min)
+                min = num;
+        }
+        return min;
     }
-    public void std(){
-
+    public double std(ArrayList<Integer>numbers){
+        //calculate std for num: numbers sum = sum + Math.pow num - average, 2
+        double avg = average(numbers);
+        double sum = 0;
+        for(int num:numbers){
+            sum = sum + Math.pow((num - avg),2);
+        }
+        // Then Math.sqrt sum/number size
+        return Math.sqrt(sum/numbers.size());
     }
     public static void main(String[] args) {
+        Solution36 sol = new Solution36();
+        Scanner input = new Scanner(System.in);
         //Create an ArrayList for numbers make them integers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        String number = null;
         //do a while number != done loop
-        //Display Enter a Number and Scan int
-        //if int does not equal done
-        //Then int num Integer parse int
-        //numbers add (int)
-        //catchException e
-        //end while loop number != done
-        //calculate average for num:numbers if average = total / number size
-        //calculate maximum for num:numbers if num > max max = num
-        //calculate minimum for num:numbers if num < min min = num
-        //calculate std for num: numbers sum = sum + Math.pow num - average, 2
-        //Then Math.sqrt sum/number size
+        do{
+            //Display Enter a Number and Scan int
+            System.out.print("Enter a number: ");
+            number = input.next();
+            //if int does not equal done
+            if (!number.equals("done")){
+                try{
+                    //Then int num Integer parse int
+                    int num = Integer.parseInt(number);
+                    //numbers add (int)
+                    numbers.add(num);
+                    //catchException e
+                }catch(NumberFormatException e) {
+                    System.out.println("Not a valid input, try again.");
+                }
+            }
+            //end while loop number != done
+        }while(!number.equals("done"));
+
         //Display Numbers: All the numbers collected from Array
+        System.out.println("Numbers: " + numbers.stream().map(String::valueOf).collect(Collectors.joining(", ")));
         //Display The average is average
+        System.out.println("The average is " + sol.average(numbers));
         //Display The minimum is minimum
+        System.out.println("The minimum is " + sol.min(numbers));
+        //Display The maximum is maximum
+        System.out.println("The maximum is " + sol.max(numbers));
         //Display The standard deviation is std
+        System.out.println("The standard deviation is " + String.format("%.2f",sol.std(numbers)));
+
 
 
 
 
     }
+
+
 }
--- exercise36/src/test/java/baseline/Solution36Test.java
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -1,7 +1,61 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution36Test {
+    Solution36 sol = new Solution36();
+
+
+    @Test
+    void test_AverageisCorrect(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
+
+        double actualResults = sol.average(numbers);
+
+        assertEquals(400,actualResults);
+
+    }
+    @Test
+    void test_MaximumisCorrect(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
+
+        assertEquals(1000,sol.max(numbers));
+    }
+    @Test
+    void test_MinimumisCorrect() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
+
+
+        assertEquals(100, sol.min(numbers));
+    }
+    @Test
+    void test_StandardDeviationisCorrect() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
+
+
+        assertEquals(353.5533905932738, sol.std(numbers));
+    }
+
+
 
 }
