2e25cbdb6c52e7eae7b6e167e681f0e8ab8abd09
Fed-Abreu <arielsey@hotmail.com>
2021-10-02 23:44:08 -0400

    exercise35 finished

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -1,15 +1,50 @@
 package baseline;
 
-public class Solution35 {
-    public void nameWinner(){
+import java.lang.reflect.Array;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+public class Solution35 {
+    private Random rand = SecureRandom.getInstanceStrong();
+
+    public Solution35() throws NoSuchAlgorithmException {
     }
-    public static void main(String[] args) {
-        //Create an ArrayList name = new ArrayList
+
+    public void nameWinner(){
+        ArrayList<String>name = new ArrayList<>();
+        String winnableName = null;
+        Scanner input = new Scanner(System.in);
         //do a loop while name isnt blank
-        //if the name isnt blank add name to the ArrayList
+        do{
+            System.out.print("Enter a name: ");
+            winnableName = input.nextLine();
+
+            //if the name isnt blank add name to the ArrayList
+            if(!winnableName.isBlank())
+                name.add(winnableName);
+
+        }while(!winnableName.isBlank());
         //Then select a random name using new Random
+
+
+
         //Then winner = random.nextInt(name.size)
+        int winner = this.rand.nextInt(name.size());
         //Display The winner is... winner
+        System.out.println("The winner is... " + name.get(winner) + ".");
+    }
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Solution35 sol = new Solution35();
+        //Create an ArrayList name = new ArrayList
+       sol.nameWinner();
+
+
     }
 }
--- exercise35/src/test/java/baseline/Solution35Test.java
+++ exercise35/src/test/java/baseline/Solution35Test.java
@@ -1,7 +1,26 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution35Test {
+
+    @Test
+    void test_nameWinnerAdds() throws NoSuchAlgorithmException {
+      Solution35 sol = new Solution35();
+        ArrayList<String> name = new ArrayList<String>();
+
+        name.add("Jone");
+
+        String expectedResult = name.get(0);
+       assertEquals(expectedResult,name);
+
+
+
+    }
 
 }
